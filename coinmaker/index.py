5ea157757a1424fe959e52c45cefe44c5b59bc96
stenberg <stenberg.alexander@hotmail.com>
2023-03-14 13:00:21 +0100

    things

--- coinmaker/index.py
+++ coinmaker/index.py
@@ -6,5 +6,17 @@ class coinMaker:
         coords = np.random.rand(10000, 2) * 2
         return coords
 
+class Coins:
+
+    def __init__(self, lat, long, city, worth = 10):
+        self.latitude = lat
+        self.longitute = long
+        self.worth = worth
+        self.city = city
+
 p1 = coinMaker
 print(p1.func())
+
+coin = Coins(123, 321, "stockholm")
+
+print(coin.latitude)
