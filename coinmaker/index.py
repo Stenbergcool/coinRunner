1723bfac99269ecafbe0403982feb9b2187bea5f
stenberg <stenberg.alexander@hotmail.com>
2023-03-16 16:32:14 +0100

    random markers generated

--- coinmaker/index.py
+++ coinmaker/index.py
@@ -1,13 +1,28 @@
 import numpy as np
+import geojson
+import random
+from shapely.geometry import shape, Point
+
 
 
 class coinMaker:
-    def func():
-        coords = np.random.rand(10000, 2) * 2
-        return coords
+    """
+    Fetches polygon shapes and randomly creates Latitude and Longitude for Coins
+    """
+    def func(geometry, num_points):
+        points = []
+        while len(points) < num_points:
+            point = Point(geometry.bounds[0] + (geometry.bounds[2] - geometry.bounds[0]) * random.random(),
+                        geometry.bounds[1] + (geometry.bounds[3] - geometry.bounds[1]) * random.random())
+            if geometry.contains(point):
+                points.append(point)
+        return points
 
-class Coins:
-
+class Coin:
+    """
+    coin class, contain latitude longitude for markers on map.
+    also worth of coin and city it is in
+    """
     def __init__(self, lat, long, city, worth = 10):
         self.latitude = lat
         self.longitute = long
@@ -15,8 +30,12 @@ class Coins:
         self.city = city
 
 p1 = coinMaker
-print(p1.func())
+with open('stockholm.geojson') as f:
+        data = geojson.load(f)
+        feature = data['features'][0]  # Access the first feature in the "features" array
+        geometry = shape(feature['geometry'])
+        print(p1.func(geometry, 1000))
 
-coin = Coins(123, 321, "stockholm")
+coin = Coin(123, 321, "stockholm")
 
 print(coin.latitude)
